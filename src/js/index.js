c5019103bcca1f4f3065b8c4133f1caafaaed3ab
Jonathan Seitz <seitz.jon@gmail.com>
2017-01-20 09:22:51 -0500

    implementing bin-packing library and moving d3 rendering logic to separate function

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,3 @@
-/*
-dependencies to add:
-  Bootstrap
-  Lodash
-
-*/
-
-
 $(document).ready(function(){
 //  $('#addInput').click(duplicate);
   $('#addTarget').click(duplicate);
@@ -17,46 +9,80 @@ $(document).ready(function(){
   };
   function calculate (evt){
     //initialize variables
-    var inputs = [],
-      targets = [],
-      width,
-      length;
+    var source = [],
+      cuts = [],
+      test;
     // gather inputs 
     $('div.inputs div').each(function(i,e){
-      width = $(e).find('.input-width').val();
-      length = $(e).find('.input-length').val();
-      inputs.push({
-        "width": _.min([width, length]),
-        "length": _.max([width, length]),
-        "area": width * length,
-        "perimeter": (2*width) + (2*length)
+      var sourceWidth = parseFloat($(e).find('.input-width').val());
+      var sourceLength = parseFloat($(e).find('.input-length').val());
+      source.push({
+        "h": _.min([sourceWidth, sourceLength]),
+        "w": _.max([sourceWidth, sourceLength]),
       });
     });
     //gather target cuts
     $('div.targets div').each(function (i,e) {
       e = $(e);
-      width = $(e).find('.target-width').val();
-      length = $(e).find('.target-length').val();
-      targets.push({
-        "width": _.min([width, length]),
-        "length": _.max([width, length]),
-        "area": width * length,
-        "perimeter": (2*width) + (2*length)
+      var cutWidth = parseFloat($(e).find('.target-width').val());
+      var cutLength = parseFloat($(e).find('.target-length').val());
+      cuts.push({
+        "long": _.max([cutWidth, cutLength]),
+        "short": _.min([cutWidth, cutLength]),
+        "label": cutWidth + 'x' + cutLength
       });
     });
+
+    console.log(cuts);
+    var packer = new Packer(width, height);
+    cuts = _.orderBy(cuts, function(o){ return o.long * o.short }, ['desc'])
+    _.each(cuts, function (e) {
+      e.h = e.long;
+      e.w = e.short;
+    });
+    test = packer.fit(cuts);
+
+    cuts = test    
+
+    console.log(cuts);
+    draw(source, cuts);
     
+  };
+
+  function draw(source, cuts) {
+    //initialize dimensions
+    var width = source[0].w,
+    height = source[0].h,
+    scale = 5,
+    margin = 20;
+
+    //clear previous data    
+    d3.selectAll('svg')
+      .remove();
+
     d3.select('#output')
       .append('svg')
-      .attr('width', 1200)
-      .attr('height', 1200);
+      .attr('width', (width * scale) + margin)
+      .attr('height', (height * scale) + margin);
 
     var svg = d3.select('svg');
 
     svg.selectAll('.source')
-      .data(inputs).enter()
+      .data(source).enter()
       .append('rect')
       .attr('class', 'source')
-      .attr('width', function(d) {return d.length * 8})
-      .attr('height', function(d) {return d.width * 8});
-  };
+      .attr('width', function (d) { return d.w * scale })
+      .attr('height', function(d){ return d.h * scale})
+      .attr('x', margin/2)
+      .attr('y', margin/2);
+
+    svg.selectAll('.cut')
+      .data(cuts).enter()
+      .append('rect')
+      .attr('class', 'cut')
+      .attr('x', function (d) { if (d.fit) { return margin/2 + (d.fit.x * scale)} })
+      .attr('y', function (d) { if (d.fit) {return margin/2 + (d.fit.y * scale)} })
+      .attr('width', function (d) { if (d.fit) {return d.w * scale}})
+      .attr('height', function (d) { if (d.fit) {return d.h * scale}});
+  }
 })
